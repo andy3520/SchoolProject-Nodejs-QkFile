50126a8540aa9f17e74f1ea07d1946ae66ccee47
Nam <trantrungnam1997vn2@gmail.com>
2018-12-03 02:42:35 +0700

    hot update baby

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const localStorage = require('node-localstorage');
 
 const router = express.Router();
 const COGNITO = require('../controllers/cognito/cognito');
-const config = require('../config/env');
 
 router.get('/', (req, res) => {
   res.render('_userFile');
@@ -21,8 +21,7 @@ router.get('/facebook', (req, res, next) => {
 });
 
 router.post('/register', (req, res) => {
-  let userPoolConfig = COGNITO.userPool(config.poolData);
-  COGNITO.registerUser(userPoolConfig, req)
+  COGNITO.registerUser(req.body)
     .then((result) => {
       res.redirect('/');
     }, (err) => {
@@ -31,33 +30,65 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  const userPoolConfig = COGNITO.userPool(config.poolData);
-  const userDataCustom = COGNITO.userData(req, userPoolConfig);
-  const cognitoUserCustom = COGNITO.cognitoUser(userDataCustom);
-  const authenticationDetailsCustom = COGNITO.authenticationDetails(req);
-  COGNITO.signin(cognitoUserCustom, authenticationDetailsCustom);
+  COGNITO.logIn(req.body.email, req.body.password)
+  .then(result => {
+    res.json(result);
+  }, error => {
+    res.json(error);
+  })
 });
 
 router.get('/validate', (req, res) => {
-  let userPoolConfig = COGNITO.userPool(config.poolData);
-  COGNITO.validateCurrentUser(userPoolConfig)
+  COGNITO.validateCurrentUser()
     .then((result) => {
       res.json(result);
     }, (err) => {
       res.json(err);
     });
+//  }
 });
 
-router.get('/getUsers', (req, res) => {
- COGNITO.GetAll(config.poolData)
+// router.get('/getUsers', (req, res) => {
+//   COGNITO.getAll(config.poolData)
+//   .then(result => {
+//     res.json(result);
+//   }, err => {
+//     res.json(err); 
+//   })
+// });
+
+router.get('/signout', (req, res) => {
+    COGNITO.signOut();
+    res.redirect('/');
+})
+
+router.get('/forgotPassword/:email', (req, res) => {
+  console.log(req);
+  COGNITO.forgotPassword(req.params.email)
   .then(result => {
     res.json(result);
-  }, err => {
-    res.json(err); 
- })
-});
-
-router.get('/forgotPassword', (req, res) => {
-  
+  }).catch(err => {
+    res.send(err);
+  })
 })
+
+router.post('/forgotPassword/:email', (req, res) => {
+  console.log(req);
+  COGNITO.confirmPassword(req.params.email, req.body.code, req.body.newPassword)
+  .then(result => {
+    res.redirect('/');
+  }).catch(err => {
+    res.send(err);
+  })
+})
+
+router.post('/updatepassword', (req, res) => {
+  COGNITO.changePassword(req.body.email, req.body.oldPassword, req.body.newPassword)
+  .then(result => {
+    res.json(result);
+  }).catch(err => {
+    res.send(err);
+  })
+})
+
 module.exports = router;
