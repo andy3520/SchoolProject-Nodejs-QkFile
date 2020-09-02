8c6dd5ffc22e45968a2478e2d98cd6731a29df69
An Nguyen Hieu Duc <38342390+andy3520@users.noreply.github.com>
2020-09-02 21:21:48 +0700

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,12 +1,12 @@
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 const env = {
-  AWS_ACCESS_KEY: 'AKIAIYAQ6MFOPKE23EDA',
-  AWS_SECRET_ACCESS_KEY: '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',
+  AWS_ACCESS_KEY: '',
+  AWS_SECRET_ACCESS_KEY: '',
   REGION: 'us-west-2',
   Bucket: 'guestfile',
   poolData : {
-    UserPoolId: 'us-west-2_YYCZS19k2',
-    ClientId: '447t3hs1nsi0t7ode1oi9af587',
+    UserPoolId: '',
+    ClientId: '',
   }
 };
 
